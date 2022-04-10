dc3eb74e87a4d4288542a47c7abcce9d2ba89289
aldimhernandez <aldana-hernandez@hotmail.com>
2022-04-10 20:47:26 -0300

    adding comment in integration/homePage.js

--- cypress/integration/homePage.js
+++ cypress/integration/homePage.js
@@ -6,11 +6,9 @@ describe('detalles', () => {
     })
 //CP001
     it('CP001 - Validar cuotas en compra de equipo - Cuotas.12 - Equipo.A52', ()=>{
-
-        var productDescription = '#installments-text';
-
         cy.search('A52');
         cy.get('#product-item-info_6115').click();
-        cy.get(productDescription).contains('12 cuotas');
+        cy.get('#installments-text').contains('12 cuotas');
+        //Pending assertion
     })
 })
